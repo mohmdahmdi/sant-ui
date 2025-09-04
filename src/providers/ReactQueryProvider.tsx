db08fed200c5e363e9ee92160388dc3b81f92a9d
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-09-04 09:53:10 +0330

    feat: add React Query Devtools and configure ReactQueryProvider to use QueryClientProvider

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,15 +1,19 @@
-import ReactQueryProvider from "@/providers/ReactQueryProvider";
+"use client"; // needed for Next.js App Router
 
-export default function RootLayout({
+import { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "@/lib/QueryClient";
+
+export default function ReactQueryProvider({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
