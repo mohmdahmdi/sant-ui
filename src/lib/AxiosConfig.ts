a686e58170f359529afb7b49caa48d65ae3bd6eb
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-09-08 10:01:00 +0330

    feat: implement token management and request/response interceptors in Axios configuration

--- src/lib/AxiosConfig.ts
+++ src/lib/AxiosConfig.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // const API_URL = global.api_url || 'default url api';
@@ -10,5 +10,62 @@ const instance: AxiosInstance = axios.create({
     "Content-type": "application/json",
   },
 });
+
+let tokenPromise: Promise<string | undefined> | null = null;
+export let globalAccessToken: string | undefined = undefined;
+
+setInterval(() => {
+  tokenPromise = null;
+  getToken();
+}, 50 * 60 * 1000);
+
+export const getToken = async (): Promise<string | undefined> => {
+  if (process.env.NODE_ENV === "development") {
+    return process.env.REACT_APP_DEVELOPMENT_TOKEN;
+  }
+  globalAccessToken = 's'
+  return;
+};
+
+const applyRequestInterceptor = (instance: AxiosInstance) => {
+  instance.interceptors.request.use(async (config) => {
+    const token =
+      typeof globalAccessToken === "string"
+        ? globalAccessToken
+        : await getToken();
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
+};
+
+interface Err {
+  Count: number;
+  Data: null;
+  message: string;
+  StatusCode: number;
+}
+
+const applyResponseInterceptor = (instance: AxiosInstance) => {
+  instance.interceptors.response.use(null, async (error: AxiosError) => {
+    const originalRequest = error.response && error.config;
+    const status = error.response ? error.response.status : null;
+    if (status === 401) {
+      tokenPromise = null;
+      getToken();
+    } else if (status === 500) {
+      console.log("مشکلی در سمت سرور رخ داده است!");
+    } else if (status === 403) {
+      const errorMassage = String(error?.response?.data);
+      console.log(errorMassage);
+    } else if (status === 400) {
+      const massage = error?.response?.data as Err;
+      console.log(massage.message);
+    }
+    return Promise.reject(originalRequest);
+  });
+};
+
+applyRequestInterceptor(instance);
+applyResponseInterceptor(instance);
 
 export default instance;
