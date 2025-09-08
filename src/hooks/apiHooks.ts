031cda243fceee048fcdf130b5ff6964989782d4
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-09-08 11:28:47 +0330

    feat: add registration and login hooks with credential handling

--- src/hooks/apiHooks.ts
+++ src/hooks/apiHooks.ts
@@ -1,7 +1,40 @@
-"use client"
+"use client";
 
-import { useQuery } from "@tanstack/react-query";
-import services from "@/services"; // dynamically resolved dev/prod
+import { useMutation, useQuery } from "@tanstack/react-query";
+import services from "@/services";
+import { credential } from "@/services/api";
+import cookieManager from "@/utils/cookieManager";
+import localStorageManager from "@/utils/localStorageManager";
+import { queryClient } from "@/lib/QueryClient";
+
+export function useRegister(credential: credential) {
+  return useMutation({
+    mutationFn: () => services.register(credential),
+    onSuccess: (data) => {
+      if (!cookieManager.isAvailable) {
+        localStorageManager.set("authToken", data.access_token);
+        return;
+      }
+      cookieManager.set("authToken", data.access_token);
+      queryClient.setQueryData(["user"], data.user);
+    },
+    onError: () => {},
+  });
+}
+
+export function useLogin(credential: credential) {
+  return useMutation({
+    mutationFn: () => services.login(credential),
+    onSuccess: (data) => {
+      if (!cookieManager.isAvailable) {
+        localStorageManager.set("authToken", data.access_token);
+        return;
+      }
+      cookieManager.set("authToken", data.access_token);
+    },
+    onError: () => {},
+  });
+}
 
 export function usePopularServiceCategories(limit?: number) {
   return useQuery({
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,35 @@
 import instance from "@/lib/AxiosConfig";
 
-export type Product = {
-  id: string;
-  name: string;
-  price: number;
+export type credential = {
+  full_name: string;
+  email?: string;
+  phone?: string;
+  password: string;
+  gender?: string;
+  birth_date?: string;
+  profile_picture?: string;
+  bio?: string;
 };
 
-export async function getProducts(): Promise<Product[]> {
-  const { data } = await instance.get("/products");
+export async function register(credential: credential) {
+  const { data }: { data: { user: credential; access_token: string } } =
+    await instance.post("/auth/register", credential);
+
   return data;
 }
 
-export async function getProduct(id: string): Promise<Product> {
-  const { data } = await instance.get(`/products/${id}`);
+export type LoginInfo = {
+  phone?: string;
+  email?: string;
+  password: string;
+};
+
+export async function login(credential: LoginInfo) {
+  const { data }: { data: { access_token: string } } = await instance.post(
+    "/auth/register",
+    credential
+  );
+
   return data;
 }
 
