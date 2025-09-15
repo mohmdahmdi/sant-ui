23044a7be97c2389262a755de0d07d009445a0a8
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-09-15 11:31:09 +0330

    feat: add react-loading-indicators package and update secondary color palette in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { usePopularServiceCategories } from "@/hooks/apiHooks";
 import FilterIcon from "../assets/svg/FilterIcon";
 import Search from "../stories/Search/Search";
 import CategoryItem from "@/components/CategoryItem";
-import HeroIcon from '../assets/svg/HeroIcon';
+import HeroIcon from "../assets/svg/HeroIcon";
 
 export default function Home() {
   const {
@@ -16,8 +16,8 @@ export default function Home() {
   const isLoading = false;
   return (
     <main className="container">
-      <section className="flex gap-x-6">
-        <div className="bg-white shadow-md w-[25%] rounded-2xl p-4 h-[85vh]">
+      <section className="flex w-full gap-x-6">
+        <div className="bg-white shadow-md w-1/4 ounded-2xl p-4 h-[85vh]">
           <div className="flex items-center gap-2 mb-3">
             <p className="text-sm text-primary-800 font-bold">فیلترها</p>
             <span className="h-px w-full bg-gray-300" />
@@ -25,7 +25,7 @@ export default function Home() {
           </div>
           <Search placeHolder="جستجو..." value="" onChange={(text) => {}} />
         </div>
-        <div className="w-[45%]">
+        <div className="w-5/12">
           {isLoading ? (
             <></>
           ) : (
@@ -50,12 +50,15 @@ export default function Home() {
           )}
           <div className="mt-6 h-[500px] w-full bg-white rounded-2xl shadow-md"></div>
         </div>
-        <div className="w-[30%] ">
+        <div className="w-1/3">
           <p className="mr-6 text-[29px] mt-">
-            با <span className="text-secondary-800 text-4xl font-extrabold">سانتال!</span> بهترین ورژن
-            خودتان را به نمایش بگذارید
+            با{" "}
+            <span className="text-secondary-800 text-4xl font-extrabold">
+              سانتال!
+            </span>{" "}
+            بهترین ورژن خودتان را به نمایش بگذارید
           </p>
-          <HeroIcon className="absolute left-0 mt-8"/>
+          <HeroIcon className="absolute left-0 top-1/2 -translate-y-1/2" />
         </div>
       </section>
     </main>
