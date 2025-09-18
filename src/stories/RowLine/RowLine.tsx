7cff5d64e292fa41d8453bac2fa6926a806cffdf
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-09-18 12:31:51 +0330

    feat: add optional className prop to RowLine component

--- src/stories/RowLine/RowLine.tsx
+++ src/stories/RowLine/RowLine.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const RowLine = ({ title }: { title: string }) => {
+const RowLine = ({ title, className }: { title: string; className?: string }) => {
   return (
-    <div className="w-full relative h-10">
+    <div className={"w-full relative h-10" + className}>
       <div className="w-full h-px bg-neutral-700 absolute top-1/2 -translate-y-1/2"></div>
       <p className="text-center font-semibold text-neutral-700 bg-background px-2 absolute left-1/2 -translate-x-1/2 translate-y-1">
         {title}
