f8c22edb7f3e0b7a2d980e976bbcc97f8074ca59
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-12-09 22:42:44 +0330

    fix: redirecting to not-found if url is not valid

--- src/app/business/profile/[id]/[[...slug]]/BusinessServices.tsx
+++ src/app/business/profile/[id]/[[...slug]]/BusinessServices.tsx
@@ -1,15 +1,22 @@
 import ServiceCard from "@/components/ServiceCard";
 import { getServicesByBusinessId } from "@/services/api";
+import { notFound, redirect } from "next/navigation";
 import React from "react";
 
 const BusinessServices = async ({ businessId }: { businessId: string }) => {
   const data = await getServicesByBusinessId(businessId);
+
+  if (!Array.isArray(data)) {
+    notFound();
+  }
   return (
-    <div className="my-20 mt-10 flex flex-col gap-y-3">
-      {data.map((item, idx) => (
-        <ServiceCard key={`business-service-${idx}`} item={item} />
-      ))}
-    </div>
+    Array.isArray(data) && (
+      <div className="my-20 mt-10 flex flex-col gap-y-3">
+        {data.map((item, idx) => (
+          <ServiceCard key={`business-service-${idx}`} item={item} />
+        ))}
+      </div>
+    )
   );
 };
 
